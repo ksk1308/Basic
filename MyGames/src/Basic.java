d6c10a169f530f527d4bdcb2c021ff2e644ac5ae
Sravan kumar Kancharla <sravankumarkancharla@Sravans-MacBook-Pro.local>
2021-05-06 17:44:15 -0400

    added some code

--- MyGames/src/Basic.java
+++ MyGames/src/Basic.java
@@ -1,10 +1,22 @@
 
 public class Basic {
 
-	public static void main(String[] args) {
-	
-		 System.out.println("Sravan");
 
-	}
+    public static void main(String[] args) {
+        string a = "somebody";
+        string b = "is here";
+        string name = "Sravan kumar kancharla";
+
+        Scanner givenname = Scanner(System.in);
+        System.Out.println("enter the name ");
+        if (givenname == name) {
+            System.out.println("Sravan" + b);
+
+        } else {
+            System.out.println(a + b);
+        }
+
+
+    }
 
 }
